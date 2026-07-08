b4c6156ada73e36e8699445098357d30b7e25c25
duy <phucduy2501@gmail.com>
2026-07-08 22:30:54 +0700

    sua backend

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL ||
-    "postgres://postgres:123456@localhost:5432/product_manager",
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
