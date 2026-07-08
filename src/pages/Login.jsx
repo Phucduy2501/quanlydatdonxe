d293ac3819cf5214f1aaf96a13c5847e72d0859f
duy <phucduy2501@gmail.com>
2026-07-08 22:21:34 +0700

    fix login api url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/api/auth/login", {
+      const res = await fetch("https://defeat-crown-tariff-ending.trycloudflare.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
